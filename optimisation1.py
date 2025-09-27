871082bdb519fe28257aae0898b11b39c1ae21e9
mokrane-hachemi <hachemimokrane54@gmail.com>
2025-09-27 12:43:25 +0100

    ajout

--- optimisation1.py
+++ optimisation1.py
@@ -112,10 +112,10 @@ st.markdown("""
 <div style="text-align: center; font-family: Tifinaghe-Ircam Unicode sans serif;">
   <p style="color: #8B4513; 
             font-weight: bold; 
-            font-size: 10px; 
+            font-size: 20px; 
             margin-top: 10px;
             text-shadow: 1px 1px 2px rgba(0,0,0,0.1);">
-   Azul  fellawen
+   ⴰⵣⵓⵍ ⴼⴻⵍⵍⴰⵡⴻⵏ
   </p>
 </div>
 """, unsafe_allow_html=True)
